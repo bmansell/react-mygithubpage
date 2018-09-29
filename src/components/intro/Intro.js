e4c544bcd99ad73cada00cf3ec8504d154fcffa4
Brianna <brianna.mansell@boldcommerce.com>
2018-09-29 14:02:13 -0500

    Update to intro page
    - New css/scss for Intro.js
    - Added a terminal-like effect and media queries for different screen sizes
    - Updated Profile.js blurb

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,31 +1,19 @@
 import React, {Component} from 'react';
-import {
-    Card,
-    CardBody,
-    CardTitle,
-    CardText
-} from 'reactstrap';
+
+import '../../css/Intro.css';
 
 class Intro extends Component {
     render() {
         return (
-            <div className='main-content'>
-                <Card>
-                    <CardBody>
-                        <CardTitle>Hello, world!</CardTitle>
-                        <CardText>
-                            I'm a recent graduate from the University of Manitoba's Computer Science Co-operative program. As a computer scientist, the user experience is a concept I value greatly and my background in psychology helps me to be a more conscious developer of this experience.
-                            <br/>
-                            <br/>
-                            In September of 2017, I attended my first <a href='https://www.canadalearningcode.ca/' target='blank' rel='noopener noreferrer'>Canada Learning Code</a> event as a volunteer mentor. It was such a rewarding experience getting to assist learners with different levels of experience and having the opportunity to introduce women of all backgrounds to my passion. Since that time I have been volunteering at other Canada Learning Code events such as the "Intro to Python" and "Building a Webpage with HTML & CSS" workshops.
-                            <br/>
-                            <br/>
-                            Recently I was asked to join the <a href='http://cs.umanitoba.ca/~wics/' target='blank' rel='noopener noreferrer'>Women of Computer Science (WICS)</a> community from the University of Manitoba, and I will be helping teach requested topics and provide support for its current members as an industry mentor. I also will be leading a Ladies Learning Code "Building a Web Store with Shopify" workshop <a href='https://www.eventbrite.ca/e/ladies-learning-code-building-a-web-store-with-shopify-winnipeg-registration-46020968957' target='blank' rel='noopener noreferrer'>on June 23rd at Bold Commerce!</a>
-                            <br/>
-                            <br/>
-                        </CardText>
-                    </CardBody>
-                </Card>
+            <div className="terminal-container">
+                <div className="terminal">
+                    <div className="css-typing">
+                        <h5>
+                            <span className="orange">briannamansell</span> at <span className="yellow">Brianna-Mansells-Website</span> in <span className="green">~/workspace</span> on <span className="blue">develop</span>
+                        </h5>
+                        <p className="type-in"><span className="user">$</span>Hello, world!</p>
+                    </div>
+                </div>
             </div>
         );
     }
